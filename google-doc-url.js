65d651e88b55a99de81abf807bfbcbdaa11323a0
bumjin <bumjin@gmail.com>
2022-07-16 17:23:26 +0900

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1onveAE1u5TE5YZnX-gsEWKqAR2zo0NzbLQ-JifgwkLE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyC2nPgmK0R7OoDgxOOApPhGN6UgbHFlkU8';
